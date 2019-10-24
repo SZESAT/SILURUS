86456e32df9591f3300a913f7798c26ccb3ad8e0
SZESAT <sze-antenna@tilb.sze.hu>
2019-10-25 01:04:38 +0200

    PI feltoltes 25/10/2019-01:04:38

--- wev/README.md
+++
@@ -1,4 +0,0 @@
-### SILURUS Antennavezérlő
-
-A repository a Széchenyi István Egyetemen folyó SZESAT projekt SILURUS antennavezérlőjét tartalmazza.
-Jelen program webes interfészt kínál a SPID MD-02 vezérlőhöz.
--- wev/static/kepfrissites.js
+++
@@ -1,8 +0,0 @@
-function reloadGraph() {
-   var now = new Date();
-
-   document.images['kamerakep'].src = 'http://cam.idokep.hu/kamera.php?user=honfyrk&' + now.getTime();
-
-   // Start new timer (1 min)
-   timeoutID = setTimeout('reloadGraph()', 5000);
-}
